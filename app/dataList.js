c9a7d195b7821e0d7bbe63bb3de08bebcdff72aa
heyiming <yiming_he@ssqian.com.cn>
2017-02-27 15:54:52 +0800

    details

--- app/dataList.js
+++ app/dataList.js
@@ -7,16 +7,16 @@ let dataList = [
         "desc": `
             
             <div class=${style.infoRow}>
-                <div>姓&emsp;&emsp;名：何一鸣</div>
+                <div>姓名：何一鸣</div>
                 <div>出生年月：1992.10</div>
             </div>
             <div class=${style.infoRow}>
-                <div>手&emsp;&emsp;机：15271831026</div>
+                <div>手机：15271831026</div>
                 <div>毕业院校：天津大学</div>
             </div>
             <div class=${style.infoRow}>
-                <div>邮&emsp;&emsp;箱：hym826@126.com</div>
-                <div>学&emsp;&emsp;历：本科</div>
+                <div>邮箱：hym826@126.com</div>
+                <div>学历：本科</div>
             </div>
             <div class=${style.infoRow}>
                 <div>求职意向：前端开发工程师</div>
@@ -47,7 +47,6 @@ let dataList = [
                     <td class=${style.w30}>助理设计师</td>
                 </tr>
             </table>
-            <br/>  
             <table class=${style.w100}>
                 <tr>
                     <td class=${style.w30}>2015.09 - 至今</td>
@@ -57,7 +56,7 @@ let dataList = [
             </table>
             <ul class=${style.itemUl}>
                 <li>负责公司产品（上上签电子签约云平台）用户中心模块的前端开发工作，包括基本信息、签名/印章管理、实名认证流程、计费、账户安全等功能的实现；</li>
-                <li>参与公司前端UI框架的制定，规范各种常用组件的样式及弹框、loading、分页器、表单等组件的样式和交互，写了一个简单的SPA将该框架及其说明部署在公司内网上；</li>
+                <li>在项目早期参与公司前端UI框架的制定，规范各种常用组件的样式及弹框、loading、分页器、表单等组件的样式和交互，使得项目开发更加规范和组件化；</li>
                 <li>微信公众号里的销售演示demo开发，在微信端实现公司产品基本签约功能，方便销售外出演示；</li>
                 <li>负责公司官网及登录注册页、产品中心等静态页面及一些市场活动页面的开发；</li>
             </ul>
@@ -69,17 +68,21 @@ let dataList = [
         "desc": `
             <div style="font-weight: bold">Web前端：</div>
             <ul class=${style.itemUl}>
-                <li>熟练使用HTML、CSS，熟悉HTML5、CSS3新特性，使用Sass编译CSS；</li>
-                <li>熟悉web前端开发流程，熟练使用JavaScript、jQuery，能够使用ES6进行前端页面开发；</li>
-                <li>会使用React，理解redux思想；</li>
+                <li>熟练使用HTML、CSS，熟悉HTML5、CSS3新特性；</li>
+                <li>熟悉web前端开发流程，熟练使用JavaScript、jQuery、ES6进行前端页面开发</li>
+                <li>熟练使用 React + webpack 开发单页web app</li>
                 <li>了解Vue、Angular等前端框架；</li>
-                <li>熟练使用Photoshop，了解webpack 等前端开发工具；</li>
-                <li>熟练使用git；</li>
             </ul>
             <div style="font-weight: bold">其他：</div>
             <ul class=${style.itemUl}>
                 <li>大学英语六级（CET-6），汉语言文学双学位；</li>
                 <li>熟练使用Office、AutoCAD;</li>
+            </ul>
+            <div><span style="font-weight: bold">作品相关：</span>（近期GitHub不稳定，如果打不开请先挂上VPN）</div>
+            <ul class=${style.itemUl}>
+                <li><a href="https://ymjrcc.github.io/" target="_blank">个人主页</a></li>
+                <li><a href="https://github.com/ymjrcc/Fireball_by_React" target="_blank">仿火球买手app</a></li>
+                <li><a href="https://github.com/ymjrcc" target="_blank">GitHub</a></li>
             </ul>
         `
     },
